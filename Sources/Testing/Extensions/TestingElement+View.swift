58560ab62ae756b92949a487f48157cbb5fce6cd
Almaz Ibragimov <almazrafi@gmail.com>
2026-05-07 19:20:43 +0300

    Add drag gesture methods with additional parameters (#6)

--- Sources/Testing/Extensions/TestingElement+View.swift
+++ Sources/Testing/Extensions/TestingElement+View.swift
@@ -45,6 +45,84 @@ extension TestingElement where Accessibility: ViewAccessibility {
         return self
     }
 
+    /// Проверяет, что фрейм компонента находится внутри фрейма другого компонента.
+    ///
+    /// - Parameters:
+    ///   - other: Другой компонент.
+    ///   - file: Файл, в котором должен произойти сбой.
+    ///           По умолчанию используется имя файла, в котором был вызван этот метод.
+    ///   - line: Номер строки, на которой должен произойти сбой.
+    ///           По умолчанию используется номер строки, на которой был вызван этот метод.
+    /// - Returns: Экземпляр тестируемого компонента.
+    @discardableResult
+    public func assertFrame<Element: ViewAccessibility>(
+        inside other: TestingElement<Element>,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> Self {
+        XCTAssertTrue(
+            frame.flatMap { frame in
+                other.frame?.contains(frame)
+            } ?? false,
+            file: file,
+            line: line
+        )
+
+        return self
+    }
+
+    /// Проверяет, что фрейм компонента содержит фрейм другого компонента.
+    ///
+    /// - Parameters:
+    ///   - other: Другой компонент.
+    ///   - file: Файл, в котором должен произойти сбой.
+    ///           По умолчанию используется имя файла, в котором был вызван этот метод.
+    ///   - line: Номер строки, на которой должен произойти сбой.
+    ///           По умолчанию используется номер строки, на которой был вызван этот метод.
+    /// - Returns: Экземпляр тестируемого компонента.
+    @discardableResult
+    public func assertFrame<Element: ViewAccessibility>(
+        contains other: TestingElement<Element>,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> Self {
+        XCTAssertTrue(
+            other.frame.flatMap { otherFrame in
+                frame?.contains(otherFrame)
+            } ?? false,
+            file: file,
+            line: line
+        )
+
+        return self
+    }
+
+    /// Проверяет, что фрейм компонента пересекается с фреймом другого компонента.
+    ///
+    /// - Parameters:
+    ///   - other: Другой компонент.
+    ///   - file: Файл, в котором должен произойти сбой.
+    ///           По умолчанию используется имя файла, в котором был вызван этот метод.
+    ///   - line: Номер строки, на которой должен произойти сбой.
+    ///           По умолчанию используется номер строки, на которой был вызван этот метод.
+    /// - Returns: Экземпляр тестируемого компонента.
+    @discardableResult
+    public func assertFrame<Element: ViewAccessibility>(
+        intersects other: TestingElement<Element>,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> Self {
+        XCTAssertTrue(
+            other.frame.flatMap { otherFrame in
+                frame?.intersects(otherFrame)
+            } ?? false,
+            file: file,
+            line: line
+        )
+
+        return self
+    }
+
     /// Проверяет, что наличие компонента равно указанному.
     ///
     /// - Parameters:
@@ -74,7 +152,7 @@ extension TestingElement where Accessibility: ViewAccessibility {
     ///
     /// - Parameters:
     ///   - frame: Фрейм компонента.
-    ///   - timeout: Время ожидания текста компонента в секундах.
+    ///   - timeout: Время ожидания фрейма компонента в секундах.
     ///              По умолчанию равен 4 секундам.
     ///   - failing: Флаг, определяющий необходимость сбоя после безуспешного ожидания.
     ///              По умолчанию флаг включен.
@@ -96,6 +174,105 @@ extension TestingElement where Accessibility: ViewAccessibility {
             timeout: timeout,
             failing: failing,
             message: "Frame of element \(element) was not equal to \(frame) within \(timeout) seconds",
+            file: file,
+            line: line
+        )
+    }
+
+    /// Ждет указанное время, пока фрейм компонента не станет находиться внутри фрейма другого компонента.
+    ///
+    /// - Parameters:
+    ///   - other: Другой компонент.
+    ///   - timeout: Время ожидания фрейма компонента в секундах.
+    ///              По умолчанию равен 4 секундам.
+    ///   - failing: Флаг, определяющий необходимость сбоя после безуспешного ожидания.
+    ///              По умолчанию флаг включен.
+    ///   - file: Файл, в котором должен произойти сбой.
+    ///           По умолчанию используется имя файла, в котором был вызван этот метод.
+    ///   - line: Номер строки, на которой должен произойти сбой.
+    ///           По умолчанию используется номер строки, на которой был вызван этот метод.
+    /// - Returns: Экземпляр тестируемого компонента.
+    @discardableResult
+    public func waitForFrame<Element: ViewAccessibility>(
+        inside other: TestingElement<Element>,
+        timeout: TimeInterval = 4,
+        failing: Bool = true,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> Self {
+        wait(
+            for: frame.flatMap { frame in
+                other.frame?.contains(frame)
+            } ?? false,
+            timeout: timeout,
+            failing: failing,
+            message: "Frame of element \(element) was not inside frame of \(other.element) within \(timeout) seconds",
+            file: file,
+            line: line
+        )
+    }
+
+    /// Ждет указанное время, пока фрейм компонента не станет содержать фрейм другого компонента.
+    ///
+    /// - Parameters:
+    ///   - other: Другой компонент.
+    ///   - timeout: Время ожидания фрейма компонента в секундах.
+    ///              По умолчанию равен 4 секундам.
+    ///   - failing: Флаг, определяющий необходимость сбоя после безуспешного ожидания.
+    ///              По умолчанию флаг включен.
+    ///   - file: Файл, в котором должен произойти сбой.
+    ///           По умолчанию используется имя файла, в котором был вызван этот метод.
+    ///   - line: Номер строки, на которой должен произойти сбой.
+    ///           По умолчанию используется номер строки, на которой был вызван этот метод.
+    /// - Returns: Экземпляр тестируемого компонента.
+    @discardableResult
+    public func waitForFrame<Element: ViewAccessibility>(
+        contains other: TestingElement<Element>,
+        timeout: TimeInterval = 4,
+        failing: Bool = true,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> Self {
+        wait(
+            for: other.frame.flatMap { otherFrame in
+                frame?.contains(otherFrame)
+            } ?? false,
+            timeout: timeout,
+            failing: failing,
+            message: "Frame of element \(element) did not contain frame of \(other.element) within \(timeout) seconds",
+            file: file,
+            line: line
+        )
+    }
+
+    /// Ждет указанное время, пока фрейм компонента не станет пересекаться с фреймом другого компонента.
+    ///
+    /// - Parameters:
+    ///   - other: Другой компонент.
+    ///   - timeout: Время ожидания фрейма компонента в секундах.
+    ///              По умолчанию равен 4 секундам.
+    ///   - failing: Флаг, определяющий необходимость сбоя после безуспешного ожидания.
+    ///              По умолчанию флаг включен.
+    ///   - file: Файл, в котором должен произойти сбой.
+    ///           По умолчанию используется имя файла, в котором был вызван этот метод.
+    ///   - line: Номер строки, на которой должен произойти сбой.
+    ///           По умолчанию используется номер строки, на которой был вызван этот метод.
+    /// - Returns: Экземпляр тестируемого компонента.
+    @discardableResult
+    public func waitForFrame<Element: ViewAccessibility>(
+        intersects other: TestingElement<Element>,
+        timeout: TimeInterval = 4,
+        failing: Bool = true,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> Self {
+        wait(
+            for: other.frame.flatMap { otherFrame in
+                frame?.intersects(otherFrame)
+            } ?? false,
+            timeout: timeout,
+            failing: failing,
+            message: "Frame of \(element) did not intersect frame of \(other.element) within \(timeout) seconds",
             file: file,
             line: line
         )
